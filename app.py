de73d499b5262cec79739d1dcf558798276b9c1f
Can Tektas <cantektas8@gmail.com>
2025-04-16 00:13:08 +0300

    degisiklik13

--- app.py
+++ app.py
@@ -102,5 +102,7 @@ def route():
     else:
         return jsonify({'error': 'Route not found', 'status': response.status_code}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
